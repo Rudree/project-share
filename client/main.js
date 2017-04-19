4eb74cb5b97335fb6443fabbada9dea11fd34d13
Rudree <rtrivedi@deakin.edu.au>
2017-04-19 20:03:19 +1000

    Add Routes

--- client/main.js
+++ client/main.js
@@ -1,11 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { Session } from 'meteor/session';
 
 import Home from '../imports/ui/HomePage.jsx';
 import Login from '../imports/ui/LoginPage.jsx';
@@ -13,19 +11,40 @@ import Register from '../imports/ui/RegistrationPage.jsx';
 import ItemPage from '../imports/ui/UploadAnItem.jsx';
 import ItemManagementPage from '../imports/manage/ItemAPI.jsx';
 import HeadnavBar from '../imports/ui/HeadNavBar.jsx';
+import CheckEmailPage from '../imports/ui/CheckEmailPage.jsx';
+import VerifiedPage from '../imports/ui/VerifiedPage.jsx'
 
-Meteor.startup( () => {
+export const App = ({ children }) => (
+  <div>
+    <HeadnavBar />
+    {children}
+  </div>
+);
+
+Meteor.startup(() => {
   render(
- <Router>
-   <div>
-      <Route exact path="" component={HeadnavBar}/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/home" component={Home}/>
-      <Route path="/register" component={Register}/>
-      <Route path ="/login" component={Login}/>
-      <Route path = "/uploaditem" component={ItemPage}/>
-      <Route path = "/manageitem" component={ItemManagementPage}/>
-      </div>
- </Router> , document.getElementById('render-home')
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="home" component={Home} />
+        <Route path="register" component={Register} />
+        <Route path="login" component={Login} />
+        <Route path="uploaditem" component={ItemPage} />
+        <Route path="manageitem" component={ItemManagementPage} />
+        <Route path="verify-email/:token" onEnter={VerifyEmail} component={Home} />
+        <Route path="check-email" component={CheckEmailPage} />
+      </Route>
+    </Router>
+    , document.getElementById('render-home')
   );
 });
+
+export function VerifyEmail(nextState, replace) {
+  if(!Accounts.verifyEmail(nextState.params.token)) {
+    replace({
+        pathname: "/verified",
+        state: { nextPathname: nextState.location.pathname }
+    });
+  }
+}
+
+
